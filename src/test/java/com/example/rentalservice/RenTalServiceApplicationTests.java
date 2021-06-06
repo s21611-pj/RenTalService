f40e89ef9004b32cd1f42353090e5feead564e42
tursonsko <turekwojtek@gmail.com>
2021-06-06 17:01:00 +0200

    Initial commit

---
+++ src/test/java/com/example/rentalservice/RenTalServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rentalservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenTalServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
